6524d74b42f9ab7752e04a8f8a5fb30c7413cec1
Deerion <162125961+Deerion@users.noreply.github.com>
2025-05-07 21:22:33 +0200

    Fix Password

--- src/middleware.js
+++ src/middleware.js
@@ -1,17 +1,20 @@
+// middleware.js
 import { NextResponse } from 'next/server'
+
+const PASSWORD = 'haslo20031819'
 
 export function middleware(request) {
     const { searchParams } = request.nextUrl
     const token = searchParams.get('token')
-    const expectedToken = process.env.ACCESS_TOKEN
 
-    if (token !== expectedToken) {
-        return new NextResponse('Brak dostępu', { status: 401 })
+    if (token !== PASSWORD) {
+        return new NextResponse('Brak dostępu', {
+            status: 401,
+        })
     }
 
     return NextResponse.next()
 }
-
 export const config = {
     matcher: ['/', '/((?!_next).*)'],
 }
