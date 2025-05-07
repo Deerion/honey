07e469e5fb9ebe27ed74032038e346222ab6099a
Deerion <162125961+Deerion@users.noreply.github.com>
2025-05-07 21:31:11 +0200

    Fix images

--- src/middleware.js
+++ src/middleware.js
@@ -4,17 +4,20 @@ import { NextResponse } from 'next/server'
 const PASSWORD = 'haslo20031819'
 
 export function middleware(request) {
-    const { searchParams } = request.nextUrl
+    const { searchParams, pathname } = request.nextUrl
+    // Jeśli ścieżka zawiera kropkę, to jest to plik statyczny – przepuść bez sprawdzania hasła
+    if (pathname.includes('.')) {
+        return NextResponse.next();
+    }
     const token = searchParams.get('token')
-
     if (token !== PASSWORD) {
         return new NextResponse('Brak dostępu', {
             status: 401,
         })
     }
-
     return NextResponse.next()
 }
+
 export const config = {
     matcher: ['/', '/((?!_next).*)'],
 }
